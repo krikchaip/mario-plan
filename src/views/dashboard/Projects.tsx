122a4a8422eceb48489df8f4e415c1a096507cd3
krikchaip <krikchai.p@gmail.com>
2019-08-12 14:15:01 +0700

    refactor projects using array pattern

--- src/views/dashboard/Projects.tsx
+++ src/views/dashboard/Projects.tsx
@@ -3,27 +3,17 @@ import React, { FC } from 'react'
 const Projects: FC = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title ">Project title</span>
-          <p>Posted by The Net Ninja</p>
-          <p className="grey-text">3rd September, 2am</p>
-        </div>
-      </div>
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title ">Project title</span>
-          <p>Posted by The Net Ninja</p>
-          <p className="grey-text">3rd September, 2am</p>
-        </div>
-      </div>
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title ">Project title</span>
-          <p>Posted by The Net Ninja</p>
-          <p className="grey-text">3rd September, 2am</p>
-        </div>
-      </div>
+      {Array(3)
+        .fill(undefined)
+        .map((_, idx) => (
+          <div key={idx} className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+              <span className="card-title ">Project title</span>
+              <p>Posted by The Net Ninja</p>
+              <p className="grey-text">3rd September, 2am</p>
+            </div>
+          </div>
+        ))}
     </div>
   )
 }
