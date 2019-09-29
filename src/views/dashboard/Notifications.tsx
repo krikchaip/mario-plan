d7644c907daa30c3e3d46ca156d257547782d650
krikchaip <krikchai.p@gmail.com>
2019-09-29 23:12:23 +0700

    🎭 mock: notification ui

--- src/views/dashboard/Notifications.tsx
+++ src/views/dashboard/Notifications.tsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 export const Notifications = () => {
   return (
-    <div>
-      <p>Notifications</p>
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Notifications</span>
+          <ul className="online-users">
+            <li>Notification</li>
+            <li>Notification</li>
+            <li>Notification</li>
+            <li>Notification</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
